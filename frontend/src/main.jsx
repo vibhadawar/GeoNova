1416b41a7eaa3aa18d395f0eb3c6b6156e5f7d9f
vibhadawar <vibhadawar06@gmail.com>
2026-06-15 08:42:52 +0530

    ...

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import "./index.css";
